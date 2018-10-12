2d823d6bec40b84d4cae1bed6317ef81e91c2bd9
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-13 01:20:57 +0200

    Added getSize() & getColor()

--- api/src/main/java/games/aternos/odessa/api/team/PlayerTeam.java
+++ api/src/main/java/games/aternos/odessa/api/team/PlayerTeam.java
@@ -1,9 +1,13 @@
 package games.aternos.odessa.api.team;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
+/**
+ * The interface Player team.
+ */
 public interface PlayerTeam {
 
     /**
@@ -12,6 +16,13 @@ public interface PlayerTeam {
      * @return the id
      */
     int getId();
+
+    /**
+     * Gets team color.
+     *
+     * @return the color
+     */
+    ChatColor getColor();
 
     /**
      * Adds player to this team.
@@ -27,7 +38,7 @@ public interface PlayerTeam {
      * @param player the player that should be removed
      * @return returns true if player was removed
      */
-     boolean removePlayer(Player player);
+    boolean removePlayer(Player player);
 
     /**
      * Gets players.
@@ -35,4 +46,11 @@ public interface PlayerTeam {
      * @return the players
      */
     List<Player> getPlayers();
+
+    /**
+     * Gets current size.
+     *
+     * @return the amount of players inside this team
+     */
+    int getSize();
 }
