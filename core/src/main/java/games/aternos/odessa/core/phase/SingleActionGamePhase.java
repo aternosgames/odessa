167c5210463fe7bb173f8f69e089afbacd181cb9
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-12 22:36:06 +0200

    Creating generic game phase implementations

---
+++ core/src/main/java/games/aternos/odessa/core/phase/SingleActionGamePhase.java
@@ -0,0 +1,45 @@
+package games.aternos.odessa.core.phase;
+
+import com.google.common.base.Preconditions;
+import games.aternos.odessa.api.Game;
+import games.aternos.odessa.api.phase.GamePhase;
+
+/**
+ * This game phase will run whatever is supplied by {@code toRun} and then
+ * instantly continue with {@code nextPhase}.
+ */
+public class SingleActionGamePhase implements GamePhase {
+
+    private Runnable toRun;
+    private GamePhase nextPhase;
+
+    /**
+     * Instantiates a new single action game phase.
+     *
+     * @param toRun     the to run
+     * @param nextPhase the next phase
+     */
+    public SingleActionGamePhase(Runnable toRun, GamePhase nextPhase) {
+        Preconditions.checkNotNull(toRun, "'toRun' cannot be null");
+        Preconditions.checkNotNull(nextPhase, "'nextPhase' cannot be null");
+
+        this.toRun = toRun;
+        this.nextPhase = nextPhase;
+    }
+
+    @Override
+    public void endPhase() {
+        /* Do nothing */
+    }
+
+    @Override
+    public void startPhase(Game game) {
+        toRun.run();
+        game.advancePhase(this.nextPhase);
+    }
+
+    @Override
+    public void update() {
+        /* Do nothing */
+    }
+}
