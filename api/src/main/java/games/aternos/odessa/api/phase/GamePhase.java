03bc2493325e7c8fc41b88dd65fc7852959849cb
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-12 17:37:20 +0200

    Add comments

--- api/src/main/java/games/aternos/odessa/api/phase/GamePhase.java
+++ api/src/main/java/games/aternos/odessa/api/phase/GamePhase.java
@@ -1,9 +1,18 @@
 package games.aternos.odessa.api.phase;
 
+/**
+ * The interface for game phases.
+ */
 public interface GamePhase {
 
+    /**
+     * End phase.
+     */
     public void endPhase();
 
+    /**
+     * Start phase.
+     */
     public void startPhase();
 
 }
