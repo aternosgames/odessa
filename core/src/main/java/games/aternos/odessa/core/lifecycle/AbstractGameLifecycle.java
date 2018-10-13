07e9a1141e1aad9b390f3f7bb65d5a63d1564dfc
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-14 01:50:39 +0200

    Made private game accessible with getter

--- core/src/main/java/games/aternos/odessa/core/lifecycle/AbstractGameLifecycle.java
+++ core/src/main/java/games/aternos/odessa/core/lifecycle/AbstractGameLifecycle.java
@@ -10,19 +10,18 @@ public abstract class AbstractGameLifecycle implements GameLifecycle {
 
     private Game game;
 
-    /**
-     * If you override this, keep in mind to call {@code super.start(Game)} so {@code game} gets initialized.
-     *
-     * @param game the game
-     */
-    @Override
-    public void start(Game game) {
+    public AbstractGameLifecycle(Game game) {
         this.game = game;
+    }
+
+    public Game getGame() {
+        return this.game;
     }
 
     @Override
     public void run() {
         //Every GameLifecycle should update the game.
         game.update();
+
     }
 }
