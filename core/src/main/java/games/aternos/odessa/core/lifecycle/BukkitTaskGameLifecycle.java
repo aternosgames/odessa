1c23146c52ed988c00f5f3ba673a13b52cdf78bd
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-14 01:51:02 +0200

    Added BukkitTaskGameLifecycleFactory

--- core/src/main/java/games/aternos/odessa/core/lifecycle/BukkitTaskGameLifecycle.java
+++ core/src/main/java/games/aternos/odessa/core/lifecycle/BukkitTaskGameLifecycle.java
@@ -1,6 +1,8 @@
 package games.aternos.odessa.core.lifecycle;
 
 import games.aternos.odessa.api.Game;
+import games.aternos.odessa.api.lifecycle.GameLifecycle;
+import games.aternos.odessa.api.lifecycle.GameLifecycleFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -15,18 +17,18 @@ public class BukkitTaskGameLifecycle extends AbstractGameLifecycle {
     /**
      * Instantiates a new abstract game lifecycle. Updates game phases every {@code period} ticks.
      *
+     * @param game   the game
      * @param period the period between updates in ticks (20 ticks = 1 second)
      */
-    public BukkitTaskGameLifecycle(long period) {
+    public BukkitTaskGameLifecycle(Game game, long period) {
+        super(game);
         this.period = period;
     }
 
     @Override
-    public void start(Game game) {
-        super.start(game);
-
+    public void start() {
         //The BukkitTaskGameLifecycle uses the bukkit scheduler for getting runnable to work
-        this.thread = Bukkit.getScheduler().runTaskTimer(game.getPlugin(), this, period, period);
+        this.thread = Bukkit.getScheduler().runTaskTimer(getGame().getPlugin(), this, period, period);
     }
 
     @Override
@@ -34,7 +36,34 @@ public class BukkitTaskGameLifecycle extends AbstractGameLifecycle {
         this.thread.cancel();
     }
 
+    /**
+     * Gets the bukkit task.
+     *
+     * @return the bukkit task
+     */
     public BukkitTask getBukkitTask() {
         return this.thread;
+    }
+
+    /**
+     * The factory for creating this lifecycle.
+     */
+    public static class BukkitTaskGameLifecycleFactory implements GameLifecycleFactory {
+
+        private long period;
+
+        /**
+         * Instantiates a new Bukkit task game lifecycle factory.
+         *
+         * @param period the period
+         */
+        public BukkitTaskGameLifecycleFactory(long period) {
+            this.period = period;
+        }
+
+        @Override
+        public GameLifecycle create(Game game) {
+            return new BukkitTaskGameLifecycle(game, period);
+        }
     }
 }
