ebbbc0279e256ca30f9f81f25af3353689521c5c
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-14 01:51:32 +0200

    Renamed SecondsGameLifecycle to ThreadedGameLifecycle

---
+++ core/src/main/java/games/aternos/odessa/core/lifecycle/ThreadedGameLifecycle.java
@@ -0,0 +1,23 @@
+package games.aternos.odessa.core.lifecycle;
+
+import games.aternos.odessa.api.Game;
+
+/**
+ * The type seconds game lifecycle.
+ */
+public class ThreadedGameLifecycle extends AbstractGameLifecycle {
+
+    public ThreadedGameLifecycle(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void start() {
+        //TODO: implement ExecutorService
+    }
+
+    @Override
+    public void stop() {
+        //TODO: implement ExecutorService
+    }
+}
