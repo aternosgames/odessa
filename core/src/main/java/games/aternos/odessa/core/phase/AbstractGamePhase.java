bc1aa7dc085683126c5dd79c799ed3f80cc6400e
Rony Tesch <ChillUpX@users.noreply.github.com>
2018-10-13 21:20:59 +0200

    Added timer functionality and javadoc

--- core/src/main/java/games/aternos/odessa/core/phase/AbstractGamePhase.java
+++ core/src/main/java/games/aternos/odessa/core/phase/AbstractGamePhase.java
@@ -4,9 +4,13 @@ import com.google.common.base.Preconditions;
 import games.aternos.odessa.api.Game;
 import games.aternos.odessa.api.phase.GamePhase;
 
+/**
+ * The type Abstract game phase.
+ */
 public abstract class AbstractGamePhase implements GamePhase {
 
     private Game game;
+    private int time;
 
     @Override
     public void startPhase(Game game) {
@@ -16,7 +20,33 @@ public abstract class AbstractGamePhase implements GamePhase {
         this.startPhase();
     }
 
+    /**
+     * Gets game.
+     *
+     * @return the game
+     */
     public Game getGame() { return this.game; }
 
+    /**
+     * Start phase.
+     */
     public abstract void startPhase();
+
+    @Override
+    public void update() {
+        time++;
+        updatePhase();;
+    }
+
+    /**
+     * Gets age of phase in seconds (counts how often {@code update()} has been called).
+     *
+     * @return the age
+     */
+    public int getAge() { return this.time; }
+
+    /**
+     * Update phase.
+     */
+    public abstract void updatePhase();
 }
