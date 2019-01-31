e6dd8adcadbf091ce742d00ab82b94a6dd0d5b61
John <dashiell@dashiell.me>
2019-01-31 11:02:28 -0500

    Startup game selection scheduled so after all plugins have registered it selects a game.

--- odessa-standard/src/main/java/games/aternos/odessa/core/OdessaEngine.java
+++ odessa-standard/src/main/java/games/aternos/odessa/core/OdessaEngine.java
@@ -1,13 +1,15 @@
 package games.aternos.odessa.core;
 
 
+import games.aternos.odessa.core.engine.StartupTask;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public class OdessaEngine extends JavaPlugin {
 
   @Override
   public void onEnable() {
-
+    BukkitTask startup = new StartupTask(this).runTaskLater(this, 60);
   }
 
 }
