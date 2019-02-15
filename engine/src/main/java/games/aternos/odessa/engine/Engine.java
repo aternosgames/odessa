ba7110a2294e6cb2fb2eedc8ecf8f6de30fbec41
John <dashiell@dashiell.me>
2019-02-14 20:48:25 -0500

    Start engine logic

--- engine/src/main/java/games/aternos/odessa/engine/Engine.java
+++ engine/src/main/java/games/aternos/odessa/engine/Engine.java
@@ -1,7 +1,25 @@
 package games.aternos.odessa.engine;
 
+import games.aternos.odessa.engine.gameserver.GameServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Engine extends JavaPlugin {
 
+  private GameServer gameServer;
+
+  private static Engine engine;
+
+  @Override
+  public void onEnable(){
+    engine = this;
+    this.gameServer = new GameServer(this);
+  }
+
+  public GameServer getGameServer() {
+    return gameServer;
+  }
+
+  public static Engine getEngine() {
+    return engine;
+  }
 }
--- engine/src/main/java/games/aternos/odessa/engine/gameserver/GameServer.java
+++ engine/src/main/java/games/aternos/odessa/engine/gameserver/GameServer.java
@@ -1,7 +1,16 @@
 package games.aternos.odessa.engine.gameserver;
 
+import org.bukkit.plugin.Plugin;
+
 public class GameServer {
 
+  private final Plugin plugin;
 
+  public GameServer(Plugin plugin) {
+    this.plugin = plugin;
+  }
 
+  public Plugin getPlugin() {
+    return plugin;
+  }
 }
