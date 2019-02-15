8b1ef2ecafc1c810985f83e83f5b5f53765d52fd
John <dashiell@dashiell.me>
2019-02-15 14:04:59 -0500

    kit object logic

--- game/src/main/java/games/aternos/odessa/gameapi/game/element/Kit.java
+++ game/src/main/java/games/aternos/odessa/gameapi/game/element/Kit.java
@@ -1,4 +1,18 @@
 package games.aternos.odessa.gameapi.game.element;
 
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
 public class Kit {
+
+  private List<ItemStack> kitItems;
+
+  private String kitName;
+
+  public Kit(String kitName, List<ItemStack> kitItems) {
+    this.kitName = kitName;
+    this.kitItems = kitItems;
+  }
+
 }
