5093f2125dc3d4f1f97460e5edeccf66786e7a90
John <dashiell@dashiell.me>
2019-02-23 21:18:06 -0500

    start scoreboard object

---
+++ engine/src/main/java/games/aternos/odessa/engine/service/scoreboard/Scoreboard.java
@@ -0,0 +1,52 @@
+package games.aternos.odessa.engine.service.scoreboard;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scoreboard {
+
+  private final ScoreboardService owner;
+
+  private List<Player> usersinBoard;
+
+  private List<String> elements;
+
+  public Scoreboard(@Nonnull ScoreboardService owner) {
+    this.owner = owner;
+    this.usersinBoard = new ArrayList<>();
+    this.elements = new ArrayList<>();
+  }
+
+  public Scoreboard(@Nonnull ScoreboardService owner, @Nonnull List<Player> usersinBoard, @NonNull List<String> elements) {
+
+    this.owner = owner;
+    this.usersinBoard = usersinBoard;
+    this.elements = elements;
+
+  }
+
+  public void addPlayer(@NonNull Player player) {
+    this.usersinBoard.add(player);
+  }
+
+  public void removePlayer(@NonNull Player player) {
+    this.usersinBoard.remove(player);
+  }
+
+  public void update(@NonNull List<String> newElements) {
+    this.elements = newElements;
+    for (Player p : usersinBoard) {
+      this.owner.setUserScoreboard(this, p);
+    }
+  }
+
+  public void update() {
+    for (Player p : usersinBoard) {
+      this.owner.setUserScoreboard(this, p);
+    }
+  }
+}
