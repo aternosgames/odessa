71bc6112e81da709af911657c76b5a7ca1a6ea8d
John <dashiell@dashiell.me>
2019-03-18 14:15:40 -0400

    Impl IoConfigurationFile Super

---
+++ engine/src/main/java/games/aternos/odessa/engine/service/ioconfiguration/IoConfigurationFile.java
@@ -0,0 +1,23 @@
+package games.aternos.odessa.engine.service.ioconfiguration;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+abstract public class IoConfigurationFile {
+
+  private final File configFile;
+  private final IoConfigurationService owner;
+  private final FileConfiguration configuration;
+
+  public IoConfigurationFile(File configFile, IoConfigurationService owner) {
+    this.configFile = configFile;
+    this.owner = owner;
+    this.configuration = YamlConfiguration.loadConfiguration(this.configFile);
+  }
+
+  public FileConfiguration getConfiguration() {
+    return configuration;
+  }
+}
--- engine/src/main/java/games/aternos/odessa/engine/service/ioconfiguration/lobby/LobbyIoConfiguration.java
+++ engine/src/main/java/games/aternos/odessa/engine/service/ioconfiguration/lobby/LobbyIoConfiguration.java
@@ -1,9 +1,13 @@
 package games.aternos.odessa.engine.service.ioconfiguration.lobby;
 
-public class LobbyIoConfiguration {
+import games.aternos.odessa.engine.service.ioconfiguration.IoConfigurationFile;
+import games.aternos.odessa.engine.service.ioconfiguration.IoConfigurationService;
 
-  public LobbyIoConfiguration() {
+import java.io.File;
 
+public class LobbyIoConfiguration extends IoConfigurationFile {
+
+  public LobbyIoConfiguration(IoConfigurationService owner) {
+    super(new File(owner.getGameApi().getDataFolder() + "/lobby", "lobby.yml"), owner);
   }
-
 }
