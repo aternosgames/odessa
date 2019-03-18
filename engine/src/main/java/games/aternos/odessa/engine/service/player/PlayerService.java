5424b174a60785e33e9e9f735f2531a01917867d
John <dashiell@dashiell.me>
2019-03-18 14:53:16 -0400

    actually start impl playerservice.

--- engine/src/main/java/games/aternos/odessa/engine/service/player/PlayerService.java
+++ engine/src/main/java/games/aternos/odessa/engine/service/player/PlayerService.java
@@ -1,11 +1,10 @@
 package games.aternos.odessa.engine.service.player;
 
-import org.sonatype.aether.spi.locator.Service;
-import org.sonatype.aether.spi.locator.ServiceLocator;
+import games.aternos.odessa.engine.service.Service;
+import games.aternos.odessa.gameapi.GameApi;
 
-public class PlayerService implements Service {
-  @Override
-  public void initService(ServiceLocator serviceLocator) {
-
+public class PlayerService extends Service {
+  protected PlayerService(GameApi gameApi) {
+    super(gameApi);
   }
 }
