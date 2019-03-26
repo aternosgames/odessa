5949205116d6a2a11fb84a0a5f74a262f5d3a491
John <dashiell@dashiell.me>
2019-03-25 21:12:10 -0400

    clean and simplify click handler

--- engine/src/main/java/games/aternos/odessa/engine/lobby/handler/LobbyPlayerClickHandler.java
+++ engine/src/main/java/games/aternos/odessa/engine/lobby/handler/LobbyPlayerClickHandler.java
@@ -10,13 +10,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 /**
  * todo: simplify and clean
  */
 public class LobbyPlayerClickHandler extends LobbyControllerOwned implements Listener {
+
+  private HashMap<Player, Arena> voted;
+
   public LobbyPlayerClickHandler(LobbyController owner) {
     super(owner);
+    voted = new HashMap<>();
   }
 
   @EventHandler
@@ -24,53 +31,62 @@ public class LobbyPlayerClickHandler extends LobbyControllerOwned implements Lis
     e.setCancelled(true);
     Player p = (Player) e.getWhoClicked();
     if (e.getInventory().getName().equals("Kit Selection")) {
-
       if (e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR) {
-        Kit selected = null;
-        for (Kit k : this.getOwner().getGameLobbySystem().getGameConfiguration().getGameKits()) {
-          if (e.getCurrentItem().getItemMeta().getDisplayName().equals(k.getKitName())) {
-            selected = k;
-          }
-        }
-
-        if (selected != null) {
-          if (!this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().containsKey(p)) {
-            this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().put(p, selected);
-            this.getOwner().getGameLobbySystem().getLobbyBoard().pushBoard(p);
-          } else {
-            this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().remove(p);
-            this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().put(p, selected);
-            this.getOwner().getGameLobbySystem().getLobbyBoard().pushBoard(p);
-          }
-
-          p.closeInventory();
-          p.sendMessage(ChatColor.BLUE + "Lobby> " + ChatColor.GRAY + "Selected Kit: " + selected.getKitName());
-        }
-
-
+        this.processKitSelection((Player) e.getWhoClicked(), e.getCurrentItem());
       }
     } else if (e.getInventory().getName().equals("Arena Vote")) {
-      /*
-      todo: remove vote if already voted
-       */
       if (e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR) {
-        Arena votedFor = null;
-        for (Arena a : this.getOwner().getGameLobbySystem().getGame().getGameConfiguration().getGameArenas()) {
-          if (e.getCurrentItem().getItemMeta().getDisplayName().equals(a.getArenaName())) {
-            votedFor = a;
-          }
-        }
-        if (votedFor != null) {
-          if (!this.getOwner().getGameLobbySystem().getArenaVote().containsKey(votedFor)) {
-            this.getOwner().getGameLobbySystem().getArenaVote().put(votedFor, 0);
-          }
-          this.getOwner().getGameLobbySystem().getArenaVote().put(votedFor, this.getOwner().getGameLobbySystem().getArenaVote().get(votedFor) + 1);
-          p.closeInventory();
-          p.sendMessage(ChatColor.BLUE + "Lobby> " + ChatColor.GRAY + "Voted for: " + votedFor.getArenaName() + " by: " + votedFor.getArenaAuthor());
-        }
-
-
+        this.processArenaSelection((Player) e.getWhoClicked(), e.getCurrentItem());
       }
     }
   }
+
+  private void processKitSelection(Player p, ItemStack currentItem) {
+    Kit selected = null;
+    for (Kit k : this.getOwner().getGameLobbySystem().getGameConfiguration().getGameKits()) {
+      if (currentItem.equals(k.getKitName())) {
+        selected = k;
+      }
+    }
+
+    if (selected != null) {
+      if (!this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().containsKey(p)) {
+        this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().put(p, selected);
+        this.getOwner().getGameLobbySystem().getLobbyBoard().pushBoard(p);
+      } else {
+        this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().remove(p);
+        this.getOwner().getGameLobbySystem().getGame().getGameData().getSelectedPlayerKits().put(p, selected);
+        this.getOwner().getGameLobbySystem().getLobbyBoard().pushBoard(p);
+      }
+
+      p.closeInventory();
+      p.sendMessage(ChatColor.BLUE + "Lobby> " + ChatColor.GRAY + "Selected Kit: " + selected.getKitName());
+    }
+  }
+
+  private void processArenaSelection(Player p, ItemStack currentItem) {
+    Arena votedFor = null;
+    for (Arena a : this.getOwner().getGameLobbySystem().getGame().getGameConfiguration().getGameArenas()) {
+      if (currentItem.equals(a.getArenaName())) {
+        votedFor = a;
+      }
+    }
+    if (votedFor != null) {
+
+      if (this.voted.containsKey(p)) {
+        this.getOwner().getGameLobbySystem().getArenaVote().put(votedFor, this.getOwner().getGameLobbySystem().getArenaVote().get(votedFor) - 1);
+        this.voted.remove(p);
+      }
+
+      if (!this.getOwner().getGameLobbySystem().getArenaVote().containsKey(votedFor)) {
+        this.getOwner().getGameLobbySystem().getArenaVote().put(votedFor, 0);
+      }
+      this.getOwner().getGameLobbySystem().getArenaVote().put(votedFor, this.getOwner().getGameLobbySystem().getArenaVote().get(votedFor) + 1);
+      this.voted.put(p, votedFor);
+      p.closeInventory();
+      p.sendMessage(ChatColor.BLUE + "Lobby> " + ChatColor.GRAY + "Voted for: " + votedFor.getArenaName() + " by: " + votedFor.getArenaAuthor());
+    }
+
+  }
+
 }
