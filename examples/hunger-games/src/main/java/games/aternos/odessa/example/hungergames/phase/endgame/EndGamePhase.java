72137801c5d290f16722be1121e4b553609978ff
Dashiell Linux <dashiell@dashiell.xyz>
2019-06-05 00:10:25 -0400

    Finish out basic end game, and game ending
    Signed-off-by: Dashiell Linux <dashiell@dashiell.xyz>

--- examples/hunger-games/src/main/java/games/aternos/odessa/example/hungergames/phase/endgame/EndGamePhase.java
+++ examples/hunger-games/src/main/java/games/aternos/odessa/example/hungergames/phase/endgame/EndGamePhase.java
@@ -8,12 +8,16 @@ import games.aternos.odessa.gameapi.game.Game;
 import games.aternos.odessa.gameapi.game.GameEndReason;
 import games.aternos.odessa.gameapi.game.GameLifecycleManager;
 import games.aternos.odessa.gameapi.game.GamePhase;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class EndGamePhase extends GamePhase {
 
   private PlayerService playerService;
 
   private GameEndReason gameEndReason;
+
+    private String winner;
 
   public EndGamePhase(@NonNull GameLifecycleManager owner, Game game) {
     super(owner, game);
@@ -25,20 +29,57 @@ public class EndGamePhase extends GamePhase {
     this.setGamePhaseRunnable(new EndGameRunnable(this));
   }
 
+    private int tick;
+
+    @Override
+    public void endPhase() {
+        this.setActive(false);
+        this.getGamePhaseRunnable().cancel();
+        GameApi.getGameApi().getServer().shutdown();
+    }
+
   @Override
   public void startPhase() {
     this.setActive(true);
-    this.setGamePhaseRunnableTask(this.getGamePhaseRunnable().runTaskTimer(GameApi.getGameApi(), 0, 20L));
+      this.gameEndReason = this.getGame().getGameData().getGameEndReason();
+
+      if (this.gameEndReason == null) {
+          this.gameEndReason = GameEndReason.ERROR;
+      }
+      this.setGamePhaseRunnableTask(
+              this.getGamePhaseRunnable().runTaskTimer(GameApi.getGameApi(), 0, 20L));
+      this.winner = this.getGame().getGameData().getPlayers().get(0).getName();
+      if (this.winner == null) {
+          this.winner = "Unknown";
+      }
   }
 
   @Override
-  public void endPhase() {
-    this.setActive(false);
-    this.getGamePhaseRunnable().cancel();
-    GameApi.getGameApi().getServer().shutdown();
+  public void hook() {
 
+      if (tick >= 10) {
+          this.endPhase();
+      }
+
+      tick++;
+      switch (this.gameEndReason) {
+          case ERROR:
+              Bukkit.broadcastMessage(ChatColor.RED + "An error caused the game to stop!");
+              break;
+          case PLAYER_WON:
+              Bukkit.broadcastMessage(ChatColor.GREEN + this.winner + " Won!");
+              break;
+          case TIME_LIMIT:
+              Bukkit.broadcastMessage(ChatColor.RED + "An admin stopped the game!");
+              break;
+          case ZERO_PLAYERS:
+              Bukkit.broadcastMessage("Error.");
+              break;
+          case ADMINISTRATIVE_STOP:
+              Bukkit.broadcastMessage("An admin stopped the game.");
+              break;
+          default:
+              Bukkit.broadcastMessage("Game over.");
+      }
   }
-
-  @Override
-  public void hook() {}
 }
